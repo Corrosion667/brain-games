569af1acd4e4f71b54a5e08518b3954067e1c9dd
Artem Nechai <lenikol95@mail.ru>
2021-06-29 16:46:28 +0300

    update the brain-calc game

---
+++ brain_games/games/calc_game.py
@@ -0,0 +1,47 @@
+"""It's a library of functions and variables for brain-calc script."""
+
+import random
+
+import prompt
+from brain_games.cli import check_answer
+
+operands = ['+', '-', '*']
+reasonable_limit_of_mental_computation = 20
+
+
+def answer(operand, num1, num2):
+    """Calculate the right answer depending on the randomised operand.
+
+    Args:
+        operand: Randomised one from operands list.
+        num1: First randomised number in game.
+        num2: second randomise numbe in game.
+
+    Returns:
+        The right answer for the game.
+    """
+    if operand == '+':
+        right_answer = num1 + num2
+    elif operand == '-':
+        right_answer = num1 - num2
+    else:
+        right_answer = num1 * num2
+    return right_answer
+
+
+def play_calc(name):  # noqa: WPS210
+    """Cycle for the brain-even script.
+
+    Args:
+        name: Name of an user.
+    """
+    for attempt in range(1, 4):
+        operand = random.choice(operands)
+        num1 = random.randint(0, reasonable_limit_of_mental_computation)
+        num2 = random.randint(0, reasonable_limit_of_mental_computation)
+        print('Question: {0} {1} {2}'.format(str(num1), operand, str(num2)))
+        right_answer = answer(operand, num1, num2)
+        users_answer = prompt.string('Your answer: ')
+        scenario = check_answer(users_answer, right_answer, name, attempt)
+        if scenario == 'loose':
+            break
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,45 +1,15 @@
 #!/usr/bin/env python3
 """It's a mini-game where user have to execute some simple math operations."""
 
-import random
-
-import prompt
 from brain_games.cli import welcome_user
+from brain_games.games.calc_game import play_calc
 
 
 def main():
     """Execute a mini-game."""
     name = welcome_user()
     print('What is the result of the expression?.')
-    operands = ['+', '-', '*']
-    attempt = 1
-    reasonable_limit_of_mental_computation = 20
-    while attempt <= 3:
-        action = random.choice(operands)
-        num1 = random.randint(0, reasonable_limit_of_mental_computation)
-        num2 = random.randint(0, reasonable_limit_of_mental_computation)
-        print('Question: {0} {1} {2}'.format(str(num1), action, str(num2)))
-        if action == '+':
-            right_answer = num1 + num2
-        elif action == '-':
-            right_answer = num1 - num2
-        else:
-            right_answer = num1 * num2
-        users_answer = prompt.string('Your answer: ')
-        if str(right_answer) != users_answer:
-            print(
-                "'{0}' is wrong answer ;(."
-                "Correct answer was '{1}'.\n"
-                "Let's try again, {2}!".format(
-                    users_answer, right_answer, name,
-                ),
-            )
-            break
-        elif right_answer == users_answer:
-            print('Correct!')
-        if attempt == 3:
-            print('Congratulations, {0}!'.format(name))
-        attempt += 1
+    play_calc(name)
 
 
 if __name__ == '__main__':
