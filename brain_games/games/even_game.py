559dc107b840afbff9260dabc7386750e3e879db
Artem Nechai <lenikol95@mail.ru>
2021-06-29 16:21:20 +0300

    update brain-even game

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -1,1 +1,24 @@
-"""It's a set of functions and variables for brain-even script."""
+"""It's a library of functions and variables for brain-even script."""
+
+import random
+
+import prompt
+from brain_games.cli import check_answer
+
+reasonable_limit_of_mental_computation = 100
+
+
+def play_even(name):
+    """Cycle for the brain-even script.
+
+    Args:
+        name: Name of an user.
+    """
+    for attempt in range(1, 4):
+        task = random.randint(0, reasonable_limit_of_mental_computation)
+        right_answer = 'yes' if task % 2 == 0 else 'no'
+        print('Question: {0}'.format(str(task)))
+        users_answer = prompt.string('Your answer: ')
+        scenario = check_answer(users_answer, right_answer, name, attempt)
+        if scenario == 'loose':
+            break
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,37 +1,15 @@
 #!/usr/bin/env python3
 """It's a mini-game where user have to answer whether number is even or not."""
 
-import random
-
-import prompt
 from brain_games.cli import welcome_user
+from brain_games.games.even_game import play_even
 
 
 def main():
     """Execute a mini-game."""
     name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    attempt = 1
-    reasonable_limit_of_mental_computation = 100
-    while attempt <= 3:
-        task = random.randint(0, reasonable_limit_of_mental_computation)
-        right_answer = 'yes' if task % 2 == 0 else 'no'
-        print('Question: {0}'.format(str(task)))
-        users_answer = prompt.string('Your answer: ')
-        if right_answer != users_answer:
-            print(
-                "'{0}' is wrong answer ;(."
-                "Correct answer was '{1}'.\n"
-                "Let's try again, {2}!".format(
-                    users_answer, right_answer, name,
-                ),
-            )
-            break
-        elif right_answer == users_answer:
-            print('Correct!')
-        if attempt == 3:
-            print('Congratulations, {0}!'.format(name))
-        attempt += 1
+    play_even(name)
 
 
 if __name__ == '__main__':
