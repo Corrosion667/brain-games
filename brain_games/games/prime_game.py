5df43d980bcb6ab6bdfb898cec42db74076ceb01
Artem Nechai <lenikol95@mail.ru>
2021-06-30 13:41:30 +0300

    update prime game

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -9,19 +9,20 @@ reasonable_limit_of_mental_computation = 50
 
 
 def check(task):
-    """Create a list for prime-check of the randomised number.
+    """Define wheter the number prime or not.
 
     Args:
-        task: Randomised number for guessing by user.
+        task: Number for guessing by user.
 
     Returns:
-        List of numbers by which randomised is divisible without a remainder.
+        Answer whether number prime or not.
     """
-    check_list = []
-    for each in range(1, task + 1):
+    if task <= 2:
+        return 'no'
+    for each in range(2, task):
         if task % each == 0:
-            check_list.append(each)
-    return len(check_list)
+            return 'no'
+    return 'yes'
 
 
 def play_prime(name):
@@ -33,7 +34,7 @@ def play_prime(name):
     for attempt in range(1, 4):
         task = random.randint(2, reasonable_limit_of_mental_computation)
         print('Question: {0}'.format(str(task)))
-        right_answer = 'yes' if check(task) == 2 else 'no'
+        right_answer = check(task)
         users_answer = prompt.string('Your answer: ')
         scenario = check_answer(users_answer, right_answer, name, attempt)
         if scenario == 'loose':
