1c773751fe6d7da535128464e264a9861bebbc0e
Artem Nechai <lenikol95@mail.ru>
2021-07-01 18:03:40 +0300

    update even-game

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -10,6 +10,11 @@ game_goal = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def game_iteration():
+    """Game logic: question and right answer for the game.
+
+    Returns:
+        Right answer for game.
+    """
     task = random.randint(0, REASONABLE_LIMIT_OF_MENTAL_COMPUTATION)
     print('Question: {0}'.format(str(task)))
     return 'yes' if task % 2 == 0 else 'no'
