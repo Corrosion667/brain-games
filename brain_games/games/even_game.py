08403f876d50b98391f5dca9a6dbc42fa8d9e5cf
Artem Nechai <lenikol95@mail.ru>
2021-07-01 17:54:17 +0300

    update brain-even with an engine

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -2,23 +2,19 @@
 
 import random
 
-import prompt
-from brain_games.cli import check_answer
+from brain_games.brain_engine import common_game
 
 REASONABLE_LIMIT_OF_MENTAL_COMPUTATION = 100
 
+game_goal = 'Answer "yes" if the number is even, otherwise answer "no".'
 
-def play_even(name):
-    """Cycle for the brain-even script.
 
-    Args:
-        name: Name of an user.
-    """
-    for attempt in range(1, 4):
-        task = random.randint(0, REASONABLE_LIMIT_OF_MENTAL_COMPUTATION)
-        right_answer = 'yes' if task % 2 == 0 else 'no'
-        print('Question: {0}'.format(str(task)))
-        users_answer = prompt.string('Your answer: ')
-        scenario = check_answer(users_answer, right_answer, name, attempt)
-        if scenario == 'loose':
-            break
+def game_iteration():
+    task = random.randint(0, REASONABLE_LIMIT_OF_MENTAL_COMPUTATION)
+    print('Question: {0}'.format(str(task)))
+    return 'yes' if task % 2 == 0 else 'no'
+
+
+def play_even():
+    """Program for the brain-even script."""
+    common_game(game_goal, game_iteration)
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 """It's a mini-game where user have to answer whether number is even or not."""
 
-from brain_games.cli import welcome_user
 from brain_games.games.even_game import play_even
 
 
 def main():
     """Execute a mini-game."""
-    print('Welcome to the Brain Games!')
-    name = welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    play_even(name)
+    play_even()
 
 
 if __name__ == '__main__':
