7898649bfed6ba4964fc93b8a3b82fb527a84e15
Artem Nechai <lenikol95@mail.ru>
2021-07-01 22:01:32 +0300

    update cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -12,39 +12,3 @@ def welcome_user():
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))
     return name
-
-
-def loose(users_answer, right_answer, name):
-    """Tell the player about loosing the game.
-
-    Args:
-        users_answer: An answer given by user.
-        right_answer: An answer which is meant to be correct.
-        name: Name of an user.
-    """
-    print(
-        f"'{users_answer}' is wrong answer ;(. "
-        + f"Correct answer was '{right_answer}'.\n"
-        + f"Let's try again, {name}!",
-    )
-
-
-def check_answer(users_answer, right_answer, name, attempt):
-    """Compare answers and execute winnig or loosing scenario.
-
-    Args:
-        users_answer: An answer given by user.
-        right_answer: An answer which is meant to be correct.
-        name: Name of an user.
-        attempt: Number of current attempt durin the game.
-
-    Returns:
-        'Loose' to breake the gaming cycle.
-    """
-    if str(right_answer) == users_answer:
-        print('Correct!')
-        if attempt == 3:
-            print('Congratulations, {0}!'.format(name))
-    else:
-        loose(users_answer, right_answer, name)
-        return 'loose'
