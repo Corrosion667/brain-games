8483667129488ebae66eb9ebd9c16aa73568c115
Artem Nechai <lenikol95@mail.ru>
2021-07-04 00:19:25 +0300

    update brain-gcd

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -19,12 +19,6 @@ def gcd(num1, num2):
     Returns:
         The greatest common divider for num1 and num2.
     """
-    if num1 == 0:
-        return num2
-    if num2 == 0:
-        return num1
-    if num1 == num2:
-        return num1
     while num1 != 0 and num2 != 0:
         if num1 > num2:
             num1 = num1 % num2  # noqa: WPS350
